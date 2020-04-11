f299cf86ad1fdd06328c9bdbeddef36866870c00
Anel CC <anel.calvo.cervantes@gmail.com>
2020-04-11 01:11:10 -0400

    [Anel] Read files from resources

--- app/src/main/java/com/anelcc/monster/MainViewModel.kt
+++ app/src/main/java/com/anelcc/monster/MainViewModel.kt
@@ -1,8 +1,15 @@
 package com.anelcc.monster
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.anelcc.monster.utilities.FileHelper
 
 
-class MainViewModel : ViewModel() {
+class MainViewModel(app: Application) : AndroidViewModel(app) {
     // TODO: Implement the ViewModel
+    init {
+        val text = FileHelper.getTextFromResources(app, R.raw.monster_data)
+        Log.i(LOG_TAG, text)
+    }
 }
--- app/src/main/java/com/anelcc/monster/utilities/FileHelper.kt
+++ app/src/main/java/com/anelcc/monster/utilities/FileHelper.kt
@@ -1,2 +1,16 @@
 package com.anelcc.monster.utilities
 
+import android.content.Context
+
+class FileHelper {
+    //In java you create Static methods in Kotlin you function member of the companions
+    companion object {
+        fun getTextFromResources(context: Context, resourceId: Int): String {
+            return context.resources.openRawResource(resourceId).use {
+                it.bufferedReader().use {
+                    it.readText()
+                }
+            }
+        }
+    }
+}
