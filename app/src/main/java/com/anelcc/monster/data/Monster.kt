971bbe27ab85f28800ceff1d66a079ec1958ec0b
Anel CC <anel.calvo.cervantes@gmail.com>
2020-04-11 09:08:57 -0400

    [Anel] Model an entity in a Kotlin data class

---
+++ app/src/main/java/com/anelcc/monster/data/Monster.kt
@@ -0,0 +1,10 @@
+package com.anelcc.monster.data
+
+data class Monster (
+    val monsterName: String,
+    val imageFile: String,
+    val caption: String,
+    val description: String,
+    val price: Double,
+    val scariness: Int
+)
