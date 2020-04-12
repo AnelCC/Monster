a8dd5ab710829d50fec3aa305e4bf36a93fcf770
Anel CC <anel.calvo.cervantes@gmail.com>
2020-04-12 15:25:10 -0400

    [Anel] Read files from assets

--- app/src/main/java/com/anelcc/monster/MainViewModel.kt
+++ app/src/main/java/com/anelcc/monster/MainViewModel.kt
@@ -9,7 +9,11 @@ import com.anelcc.monster.utilities.FileHelper
 class MainViewModel(app: Application) : AndroidViewModel(app) {
     // TODO: Implement the ViewModel
     init {
-        val text = FileHelper.getTextFromResources(app, R.raw.monster_data)
-        Log.i(LOG_TAG, text)
+        val resourcesText = FileHelper.getTextFromResources(app, R.raw.monster_data)
+        Log.i(LOG_TAG, "Resources: $resourcesText")
+
+
+        val assetsText = FileHelper.getTextFromAssets(app, "monster_data.json")
+        Log.i(LOG_TAG, "Assets: $assetsText")
     }
 }
--- app/src/main/java/com/anelcc/monster/utilities/FileHelper.kt
+++ app/src/main/java/com/anelcc/monster/utilities/FileHelper.kt
@@ -5,8 +5,17 @@ import android.content.Context
 class FileHelper {
     //In java you create Static methods in Kotlin you function member of the companions
     companion object {
+
         fun getTextFromResources(context: Context, resourceId: Int): String {
             return context.resources.openRawResource(resourceId).use {
+                it.bufferedReader().use {
+                    it.readText()
+                }
+            }
+        }
+
+        fun getTextFromAssets(context: Context, fileName: String): String {
+            return context.assets.open(fileName).use {
                 it.bufferedReader().use {
                     it.readText()
                 }
