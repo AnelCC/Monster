b36581a6535cf69a40b4960713e2af3ec9d0088c
Anel CC <anel.calvo.cervantes@gmail.com>
2020-04-15 01:12:53 -0400

    [Anel] Network permissions and status

--- app/src/main/java/com/anelcc/monster/data/MonsterRepository.kt
+++ app/src/main/java/com/anelcc/monster/data/MonsterRepository.kt
@@ -1,7 +1,11 @@
 package com.anelcc.monster.data
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.anelcc.monster.LOG_TAG
 import com.anelcc.monster.utilities.FileHelper
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
@@ -20,6 +24,8 @@ class MonsterRepository(val app: Application) {
 
     init {
         getMonsterData()
+        Log.i(LOG_TAG, "Anel Network available: ${networkAvailable()}")
+
     }
 
     fun getMonsterData() {
@@ -27,5 +33,13 @@ class MonsterRepository(val app: Application) {
         val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
         val adapter: JsonAdapter<List<Monster>> = moshi.adapter(listType)
         monsterData.value = adapter.fromJson(assetsText) ?: emptyList()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun networkAvailable(): Boolean {
+        val connectivityManager = app.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo?.isConnectedOrConnecting ?: false
     }
 }
