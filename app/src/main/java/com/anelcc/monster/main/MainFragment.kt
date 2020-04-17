ea733567b45c6d11cc6803112e0cb97fb3d67d41
Anel CC <anel.calvo.cervantes@gmail.com>
2020-04-17 19:37:11 -0400

    [Anel] 2.Create and navigate to a detail fragment

--- app/src/main/java/com/anelcc/monster/main/MainFragment.kt
+++ app/src/main/java/com/anelcc/monster/main/MainFragment.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.anelcc.monster.LOG_TAG
@@ -24,10 +26,15 @@ class MainFragment : Fragment(), MainRecyclerAdapter.MonsterItemListener {
     private lateinit var viewModel: MainViewModel
     private lateinit var recyclerView: RecyclerView
     private lateinit var swipeLayout: SwipeRefreshLayout
+    private lateinit var navController: NavController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
         recyclerView = view.findViewById(R.id.recyclerView)
+        navController = Navigation.findNavController(
+            requireActivity(), R.id.nav_host
+        )
+
         swipeLayout = view.findViewById(R.id.swipeLayout)
         swipeLayout.setOnRefreshListener {
             viewModel.refreshData()
