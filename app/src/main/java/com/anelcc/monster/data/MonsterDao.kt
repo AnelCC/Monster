ba4e837b01403f64aa01a76e504fff5cd798fbc4
Anel CC <anel.calvo.cervantes@gmail.com>
2020-04-20 18:06:22 -0400

    [Anel] 2. Define SQL operations in a DAO interface

---
+++ app/src/main/java/com/anelcc/monster/data/MonsterDao.kt
@@ -0,0 +1,20 @@
+package com.anelcc.monster.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface MonsterDao {
+    @Query("SELECT * from monsters")
+    fun getAll(): List<Monster>
+
+    @Insert
+    suspend fun insertMonster(monster: Monster)
+
+    @Insert
+    suspend fun insertMonsters(monsters: List<Monster>)
+
+    @Query("DELETE from monsters")
+    suspend fun deleteAll()
+}
